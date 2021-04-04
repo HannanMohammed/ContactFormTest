86ab2a291ecf47d7121506f8de474a6603c3c6eb
HannanMohammed <77784446+HannanMohammed@users.noreply.github.com>
2021-04-04 12:08:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contactform">
+    <h2>Contact Us!</h2>
+        <form action="" method="POST">
+        <input type="text" name="name" placeholder="Your Name">
+        <input type="text" name="phone" placeholder="Phone Number">
+        <input type="email" name="email" placeholder="Email">
+        <textarea name="message" placeholder="Write your message here!"></textarea>
+        <button class="submit" type="submit">Submit!</button>
+        </form>
+    </div>
+</body>
+</html>
